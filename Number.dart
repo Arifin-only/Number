31c925183deea5d638e3bf8cab115e867387180b
Arifin-only <arifin180104@gmail.com>
2024-10-07 12:02:14 +0800

    Create Number.dart

---
+++ Number.dart
@@ -0,0 +1,19 @@
+
+ void main() {
+   int number1 = 10;
+   double number2 = 3.14;
+
+   print(number1);
+   print(number2);
+}
+
+
+//Contoh
+
+//void main(){
+//   num number = 10;
+//   print(number);
+
+//   number = 3.14;
+//   print(number);
+// }
